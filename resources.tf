95ceab801e405f5fec1fd802fb3e89a9d4b44e97
Vadym Traida <vadym_traida@epam.com>
2018-12-17 17:29:18 +0200

    add security group

--- resources.tf
+++ resources.tf
@@ -7,10 +7,41 @@ resource "aws_vpc" "vpc_testing" {
   }
 }
 resource "aws_subnet" "subnet1" {
-  cidr_block = "${cidrsubnet(aws_vpc.vpc_testing.cidr_block, 3, 1)}"
+cidr_block = "${cidrsubnet(aws_vpc.vpc_testing.cidr_block, 3, 1)}"
   vpc_id = "${aws_vpc.vpc_testing.id}"
   availability_zone = "eu-central-1a"
   tags {
       Name = "vpc_testing"
   }
 }
+
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.vpc_testing.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.vpc_testing.id}"
+  availability_zone = "eu-central-1b"
+  tags {
+      Name = "vpc_testing2"
+  }
+}
+resource "aws_subnet" "subnet3" {
+  cidr_block = "10.0.88.0/24"
+  vpc_id = "${aws_vpc.vpc_testing.id}"
+  availability_zone = "eu-central-1b"
+  tags {
+      Name = "vpc_testing3"
+  }
+}
+resource "aws_security_group" "http_in_80" {
+  vpc_id = "${aws_vpc.vpc_testing.id}"
+  ingress = {
+    cidr_blocks = ["${aws_vpc.vpc_testing.cidr_block}"]
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+   tags {
+      Name = "vpc_testing"
+  }
+} 
+
+
