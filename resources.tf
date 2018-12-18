1ae4076c1bc30639dcaae432ea62f143178771cf
Vadym Traida <vadym_traida@epam.com>
2018-12-18 19:20:25 +0200

    terraform format

--- resources.tf
+++ resources.tf
@@ -1,50 +1,58 @@
 resource "aws_vpc" "vpc_testing" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
+
   tags {
-      Name = "vpc_testing"
+    Name = "vpc_testing"
   }
 }
+
 resource "aws_subnet" "subnet1" {
-cidr_block = "${cidrsubnet(aws_vpc.vpc_testing.cidr_block, 3, 1)}"
-  vpc_id = "${aws_vpc.vpc_testing.id}"
+  cidr_block        = "${cidrsubnet(aws_vpc.vpc_testing.cidr_block, 3, 1)}"
+  vpc_id            = "${aws_vpc.vpc_testing.id}"
   availability_zone = "eu-central-1a"
+
   tags {
-      Name = "vpc_testing"
+    Name = "vpc_testing"
   }
 }
 
 resource "aws_subnet" "subnet2" {
-  cidr_block = "${cidrsubnet(aws_vpc.vpc_testing.cidr_block, 2, 2)}"
-  vpc_id = "${aws_vpc.vpc_testing.id}"
+  cidr_block        = "${cidrsubnet(aws_vpc.vpc_testing.cidr_block, 2, 2)}"
+  vpc_id            = "${aws_vpc.vpc_testing.id}"
   availability_zone = "eu-central-1b"
+
   tags {
-      Name = "vpc_testing2"
+    Name = "vpc_testing2"
   }
 }
+
 resource "aws_subnet" "subnet3" {
-  cidr_block = "10.0.88.0/24"
-  vpc_id = "${aws_vpc.vpc_testing.id}"
+  cidr_block        = "10.0.88.0/24"
+  vpc_id            = "${aws_vpc.vpc_testing.id}"
   availability_zone = "eu-central-1b"
+
   tags {
-      Name = "vpc_testing3"
+    Name = "vpc_testing3"
   }
 }
+
 resource "aws_security_group" "http_in_80" {
   vpc_id = "${aws_vpc.vpc_testing.id}"
+
   ingress = {
-#fol local access
-#    cidr_blocks = ["${aws_vpc.vpc_testing.cidr_block}"]
-#for internet acess    
+    #fol local access
+    #    cidr_blocks = ["${aws_vpc.vpc_testing.cidr_block}"]
+    #for internet acess    
     cidr_blocks = ["0.0.0.0/0"]
+
     from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    to_port   = 80
+    protocol  = "tcp"
   }
-   tags {
-      Name = "vpc_testing"
+
+  tags {
+    Name = "vpc_testing"
   }
-} 
-
-
+}
--- servers.tf
+++ servers.tf
@@ -1,21 +1,27 @@
 data "aws_ami" "ubuntu" {
- most_recent = true
- filter {
-     name = "name"
-     values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
- }
- filter {
-     name = "virtualization-type"
-     values=["hvm"]
- }
- #canonical
- owners = ["099720109477"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  #canonical
+  owners = ["099720109477"]
 }
+
 resource "aws_instance" "server" {
- ami = "${data.aws_ami.ubuntu.id}" 
- instance_type = "t2.micro"
- tags {
-     Name = "identifier"
- }
-subnet_id = "${aws_subnet.subnet3.id}" 
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "identifier"
+  }
+
+  subnet_id = "${aws_subnet.subnet3.id}"
 }
